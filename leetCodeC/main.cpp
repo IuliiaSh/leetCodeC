872ad48bdb029fde70bbdd2c994213a73564ad14
Iuliia Shevchuk <shevchuk.iuliia@gmail.com>
2021-03-01 13:22:28 +0000

    Initial Commit

---
+++ leetCodeC/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  leetCodeC
+//
+//  Created by Iuliia Shevchuk on 01/03/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
